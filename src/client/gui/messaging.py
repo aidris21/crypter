f6a7db7d0a4181fd6f0a08887d40c63c6a6f74be
Amir Idris <aai2125@columbia.edu>
2021-02-27 21:05:17 -0500

    Messaging template

--- src/client/gui/messaging.py
+++ src/client/gui/messaging.py
@@ -1,1 +1,17 @@
 import tkinter as tk
+
+class Messaging:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.width = 50
+        
+        self.frame.pack(fill=tk.X)
+        self.master.mainloop()
+
+
+    
+
+if __name__ == "__main__":
+    root = tk.Tk(className="crypter")
+    app = Messaging(root)
